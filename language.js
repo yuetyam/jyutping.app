6ec1280b03f549ef1b0d024e310d05f3b30df071
Bing ZHEUNG <bing@zheung.org>
2024-06-24 00:01:27 +0800

    Improve language.js

--- language.js
+++ language.js
@@ -3,9 +3,19 @@ selectElement.addEventListener("change", (event) => {
         const code = event.target.value;
         const url = new URL(window.location.href);
         if (code) {
-                url.pathname = url.pathname.replace("/cmn/", `/${code}/`).replace("/en/", `/${code}/`);
+                if (url.pathname.startsWith("/cmn/")) {
+                        url.pathname = url.pathname.replace("/cmn/", `/${code}/`)
+                } else if (url.pathname.startsWith("/en/")) {
+                        url.pathname = url.pathname.replace("/en/", `/${code}/`)
+                } else {
+                        url.pathname = "/" + code + url.pathname
+                }
         } else {
-                url.pathname = url.pathname.replace("/cmn/", '/').replace("/en/", '/');
+                if (url.pathname.startsWith("/cmn/")) {
+                        url.pathname = url.pathname.replace("/cmn/", '/')
+                } else if (url.pathname.startsWith("/en/")) {
+                        url.pathname = url.pathname.replace("/en/", '/')
+                }
         }
         url.pathname = url.pathname.replace(/\/+$/, '');
         window.location.href = url.toString().replace(/\/+$/, '');
