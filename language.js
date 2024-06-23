90fd8806df056b7a91c9ef5f6bbb5f9bec7ee678
Bing ZHEUNG <bing@zheung.org>
2024-06-23 23:45:00 +0800

    Improve language.js

--- language.js
+++ language.js
@@ -2,13 +2,11 @@ const selectElement = document.querySelector(".language");
 selectElement.addEventListener("change", (event) => {
         const code = event.target.value;
         const url = new URL(window.location.href);
-        let pathParts = url.pathname.split('/').filter(part => part !== '');
-        if (pathParts[0] && ['cmn', 'en'].includes(pathParts[0])) {
-                pathParts[0] = code
-        } else if (code) {
-                pathParts.unshift(code);
+        if (code) {
+                url.pathname = url.pathname.replace("/cmn/", `/${code}/`).replace("/en/", `/${code}/`);
+        } else {
+                url.pathname = url.pathname.replace("/cmn/", '/').replace("/en/", '/');
         }
-        url.pathname = '/' + pathParts.join('/').replace(/\/+$/, '');
         url.pathname = url.pathname.replace(/\/+$/, '');
         window.location.href = url.toString().replace(/\/+$/, '');
 });
