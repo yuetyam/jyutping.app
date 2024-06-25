0d115da07d376e40f813395627e7c62044a23589
Bing ZHEUNG <bing@zheung.org>
2024-06-25 14:42:34 +0800

    Improve language.js

--- language.js
+++ language.js
@@ -2,21 +2,27 @@ const selectElement = document.querySelector(".language");
 selectElement.addEventListener("change", (event) => {
         const code = event.target.value;
         const url = new URL(window.location.href);
+        const pn = url.pathname;
         if (code) {
-                if (url.pathname.startsWith("/cmn/")) {
-                        url.pathname = url.pathname.replace("/cmn/", `/${code}/`)
-                } else if (url.pathname.startsWith("/en/")) {
-                        url.pathname = url.pathname.replace("/en/", `/${code}/`)
+                if (pn.startsWith("/cmn/")) {
+                        const regex = /^\/cmn\//;
+                        url.pathname = pn.replace(regex, `/${code}`);
+                } else if (pn.startsWith("/en/")) {
+                        const regex = /^\/en\//;
+                        url.pathname = pn.replace(regex, `/${code}`);
                 } else {
-                        url.pathname = "/" + code + url.pathname
+                        url.pathname = "/" + code + pn;
                 }
         } else {
-                if (url.pathname.startsWith("/cmn/")) {
-                        url.pathname = url.pathname.replace("/cmn/", '/')
-                } else if (url.pathname.startsWith("/en/")) {
-                        url.pathname = url.pathname.replace("/en/", '/')
+                if (pn.startsWith("/cmn/")) {
+                        const regex = /^\/cmn\//;
+                        url.pathname = pn.replace(regex, '/');
+                } else if (pn.startsWith("/en/")) {
+                        const regex = /^\/en\//;
+                        url.pathname = pn.replace(regex, '/');
                 }
         }
-        url.pathname = url.pathname.replace(/\/+$/, '');
-        window.location.href = url.toString().replace(/\/+$/, '');
+        const trailingSlash = /\/+$/;
+        url.pathname = url.pathname.replace(trailingSlash, '');
+        window.location.href = url.toString().replace(trailingSlash, '');
 });
